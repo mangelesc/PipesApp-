5c1bd8fe2171c9366bdf2a8936fb3d39043b3814
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-11-06 19:37:32 +0100

    first pipes

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 
-import { MenuModule } from 'primeng/menu';
+// import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
+import { PanelModule } from 'primeng/panel';
+
 
 @NgModule({
 
   exports: [
-    MenuModule,
+    // MenuModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
     MenubarModule,
+    PanelModule,
   ]
 })
 
