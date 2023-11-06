84c05e4295d18a196571b2dc75fd9938c4d91df1
M Angeles Cordoba <mangeles.cg91@gmail.com>
2023-11-06 20:21:35 +0100

    Set global locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 
-// Config del locale de la app 
+// Config del locale de la app (no global global)
 import localeEs from '@angular/common/locales/es-419';
 
 import { registerLocaleData } from '@angular/common';
@@ -25,7 +25,12 @@ registerLocaleData( localeEs )
     
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    // Locale de forma global 
+    { 
+      provide: LOCALE_ID, useValue: 'es-419' 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
